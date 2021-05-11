6f2f6c091411f1115fe8ddcf1d4dd56582830ea5
suthirak <suthirakprom81@gmail.com>
2021-05-11 13:09:29 +0700

    Finished Product

--- AttendanceProject.py
+++ AttendanceProject.py
@@ -2,6 +2,7 @@ import cv2
 import numpy as np
 import face_recognition
 import os
+from datetime import datetime
 
 path = 'ImagesAttendance'
 images = []
@@ -14,6 +15,7 @@ for cl in myList:
     classNames.append(os.path.splitext(cl)[0])
 print(classNames)
 
+
 def findEncodings(images):
     encodeLists = []
     for img in images:
@@ -21,6 +23,20 @@ def findEncodings(images):
         encode = face_recognition.face_encodings(img)[0]
         encodeLists.append(encode)
     return encodeLists
+
+
+def markAttendance(name):
+    with open('Attendance.csv', 'r+') as f:
+        myDataList = f.readlines()
+        nameList = []
+        for line in myDataList:
+            entry = line.split(',')
+            nameList.append(entry[0])
+        if name not in nameList:
+            now = datetime.now()
+            dtString = now.strftime('%H:%M:%S')
+            f.writelines(f'\n{name}, {dtString}')
+
 
 encodeListKnow = findEncodings(images)
 print("Encoding Completed")
@@ -43,12 +59,13 @@ while True:
 
         if matches[matchIndex]:
             name = classNames[matchIndex].upper()
-            print(name)
+            # print(name)
             y1, x2, y2, x1 = faceLoc
-            y1, x2, y2, x1 = y1*4, x2*4, y2*4, x1*4
+            y1, x2, y2, x1 = y1 * 4, x2 * 4, y2 * 4, x1 * 4
             cv2.rectangle(img, (x1, y1), (x2, y2), (0, 255, 0), 2)
-            cv2.rectangle(img, (x1, y2-35), (x2, y2), (0, 255, 0), cv2.FILLED)
-            cv2.putText(img, name, (x1+6, y2-6), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 255, 255), 2)
+            cv2.rectangle(img, (x1, y2 - 35), (x2, y2), (0, 255, 0), cv2.FILLED)
+            cv2.putText(img, name, (x1 + 6, y2 - 6), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 255, 255), 2)
+            markAttendance(name)
 
     cv2.imshow("Webcam", img)
     cv2.waitKey(1)
